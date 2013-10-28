0f3f651cae3728e70e10c5e37aecefae0ef45fd1
paragjain16 <pjain11@illinois.edu>
2013-10-28 16:34:02 -0500

    Added some files

---
+++ DSKeyValueStore/src/org/ds/server/Gossiper.java
@@ -0,0 +1,158 @@
+package org.ds.server;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Random;
+import java.util.Set;
+
+import org.ds.logger.DSLogger;
+import org.ds.member.Member;
+
+/**
+ * @author pjain11, mallapu2
+ * A thread of this class is invoked every 500 ms 
+ * this thread randomly chooses a member from 
+ * the member list and gossips its member list 
+ *
+ */
+public class Gossiper implements Runnable{
+	private HashMap<String, Member> aliveMembers;
+	private HashMap<String, Member> deadMembers;
+	private Object lockUpdateMember;
+	private Member itself;
+	private ArrayList<Member> memberList;
+	private DatagramSocket socket;
+	private ArrayList<String> keysToRemove;
+	
+	public Gossiper(HashMap<String, Member> aliveMembers, HashMap<String, Member> deadMembers, Object lockUpdateMember, Member itself){
+		this.aliveMembers = aliveMembers;
+		this.deadMembers = deadMembers;
+		this.lockUpdateMember = lockUpdateMember;
+		this.itself = itself;
+		//this.socket = socket;
+	}
+	
+	public void run(){	
+		DSLogger.log("Gossiper", "run", "Entered");
+		//Acquire a lock on membership list
+		synchronized(lockUpdateMember){
+			DSLogger.log("Gossiper", "run", "Lock Acquired by gossiper");
+			//Increment and update its own heartbeat and timestamp
+			itself.incrementHB();
+			itself.setTimeStamp(new Date().getTime());
+			aliveMembers.put(itself.getIdentifier(), itself);
+			DSLogger.log("Gossiper", "run", itself.getIdentifier()+" added to member list");
+			Set<String> keys = aliveMembers.keySet();
+			Member aMember;
+			keysToRemove = new ArrayList<String>();
+			for(String key: keys){
+				aMember =aliveMembers.get(key);
+				//Check members for timeout
+				if(aMember.checkTimeOut()){
+					keysToRemove.add(aMember.getIdentifier());
+					// At boot up the contact machine's ip address will be read from 
+					// a local file so it will not have a id which is a 
+					// combination of identifier given to it during start up +"#"+ ip address.
+					// Thus the contact machine's id will start with # for now so don't put it
+					// in dead list now... this id will be updated when the contact machine
+					// sends first gossip to this machine with its complete identifier
+					// Logic for updating id is in Receiver.java class
+					if(!aMember.getIdentifier().startsWith("#")){
+						deadMembers.put(aMember.getIdentifier(), aMember);
+						DSLogger.report(aMember.getIdentifier()," added to dead list");
+					}
+				}
+			}
+			for(String keytoRemove: keysToRemove){
+				aliveMembers.remove(keytoRemove);
+				DSLogger.log("Gossiper", "run", keytoRemove+" removed from alive list");
+			}
+			DSLogger.log("Gossiper", "run", "Alive and dead member list updated");
+			memberList = new ArrayList<Member>(aliveMembers.values());
+		
+			DSLogger.log("Gossiper", "run", "Lock released by gossiper");
+			
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			ObjectOutputStream oos = null;
+			try {
+				socket = new DatagramSocket();
+				oos = new ObjectOutputStream(baos);
+				oos.writeObject(memberList);
+				byte[] buf = baos.toByteArray();
+				/* Using expression 2 seconds  = Log N base k where N is no of machines and k is no of machines to gossip
+				 * As out timeout time is 3 seconds and 5 seconds is the deadline in which the 
+				 * gossip of new dead or alive member should spread thats why 
+				 * taking using sqrt of no of machines multiplied by gossip time period in seconds */
+
+				int noOfMachinestoGossip = (int)Math.floor(Math.sqrt(memberList.size()*0.5));
+				
+				if(noOfMachinestoGossip < 1){ // Gossip to atleast one machine
+					noOfMachinestoGossip=1;
+				}
+				DSLogger.log("Gossiper", "run", "No of members to gossip "+noOfMachinestoGossip);
+				for(int i=0; i < noOfMachinestoGossip; i++ ){
+					Member memberToGossip = chooseRandom();
+					printGossip(memberToGossip);
+					if(memberToGossip!=null){
+						DatagramPacket packet = new DatagramPacket(buf, buf.length, memberToGossip.getAddress(), memberToGossip.getPort());
+						socket.send(packet);
+					}
+				}
+				DSLogger.log("Gossiper", "run", "Exiting gossiper");
+			}catch (IOException e) {
+				DSLogger.log("Gossiper", "run", e.getMessage());
+				e.printStackTrace();
+			}finally{
+				socket.close();
+			}
+		}
+	}
+	//Choose a random member to gossip.. try 15 times if the chosen member is itself otherwise return null
+	public Member chooseRandom(){
+		DSLogger.log("Gossiper", "chooseRandom", "Choosing a Random member");
+		Random random = new Random();
+		int tryAnother = 15;
+		while(tryAnother-- >0){
+			int i = random.nextInt(memberList.size());
+			DSLogger.log("Gossiper", "chooseRandom", "Random "+memberList.get(i).getIdentifier());
+			if(!(memberList.get(i) == itself)){
+				DSLogger.log("Gossiper", "chooseRandom", "Member "+memberList.get(i).getIdentifier()+" chosen to gossip");
+				return memberList.get(i);
+			}
+		}
+		DSLogger.log("Gossiper", "run", "No members to choose");
+		return null;
+		
+	}
+	
+	
+	/*Print Gossip method*/
+	public void printGossip(Member mem){
+		if(mem!=null){
+			System.out.println("Gossiping to "+mem.getIdentifier());
+		}
+		System.out.println("Alive Members ---------------------------- Local Time " + new Date());
+		Set<String> keys = aliveMembers.keySet();
+		Member aMember;
+		for(String key: keys){
+			aMember =aliveMembers.get(key);
+			if(!aMember.getIdentifier().startsWith("#")){
+				System.out.println(aMember.getIdentifier());
+			}
+		}
+		System.out.println("Dead Members ----------------------------- Local Time "+ new Date());
+		keys = deadMembers.keySet();
+		for(String key: keys){
+			aMember =deadMembers.get(key);
+			if(!aMember.getIdentifier().startsWith("#")){
+				System.out.println(aMember.getIdentifier());
+			}
+		}
+	}
+}
---
+++ DSKeyValueStore/src/org/ds/server/Receiver.java
@@ -0,0 +1,253 @@
+package org.ds.server;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.ds.logger.DSLogger;
+import org.ds.member.Member;
+/**
+ * 
+ * @author pjain11,mallapu2
+ * Class responsible for receiver thread which updates the local membership list maintained in the node 
+ * with the membership list received over the network.
+ */
+public class Receiver implements Runnable {
+
+	private Map<String, Member> aliveMap;
+	private Map<String, Member> deadMap;
+	private DatagramSocket nodeSocket;
+	private Object nodeLockObject;
+
+	public Receiver(Map<String, Member> aliveMap, Map<String, Member> deadMap,
+			DatagramSocket nodeSocket, Object nodeLockObject) {
+		super();
+		this.nodeSocket = nodeSocket;
+		this.aliveMap = aliveMap;
+		this.deadMap = deadMap;
+		this.nodeLockObject = nodeLockObject;
+	}
+
+	@Override
+	public void run() {
+		DSLogger.log("Receiver", "run", "Entered Run");
+		byte[] msgBuffer = new byte[2048];
+		DatagramPacket msgPacket = new DatagramPacket(msgBuffer,
+				msgBuffer.length);
+		while (true) {
+
+			try {
+				DSLogger.log("Receiver", "run", "Waiting to receive UDP data");
+
+				nodeSocket.receive(msgPacket);
+
+				ByteArrayInputStream bis = new ByteArrayInputStream(
+						msgPacket.getData());
+				ObjectInputStream ois = new ObjectInputStream(bis);
+
+				Object memberList = ois.readObject();
+
+				if (memberList instanceof List<?>) {
+					List<Member> memList = (List<Member>) memberList;
+					DSLogger.log("Receiver", "run",
+							"Received member list of size: " + memList.size());
+					for (Member mem : memList) {
+						DSLogger.log("Receiver", "run", "Received member:  "
+								+ mem.getIdentifier() + " with heartbeat:"
+								+ mem.getHeartBeat());
+					}
+					synchronized (nodeLockObject) {
+						DSLogger.log("Receiver", "run",
+								"Lock Acquired by receiver");
+						for (Member member : memList) { // Iterate over the
+														// member
+														// list
+														// received over the
+														// network
+
+							String memAddress = member.getIdentifier();
+
+							/*
+							 * If its a contact machine address then update its
+							 * id as this node has just started and only knows
+							 * the ip address of contact machine
+							 */
+							if (aliveMap.containsKey("#"
+									+ member.getAddress().getHostAddress())) {
+								aliveMap.remove("#"
+										+ member.getAddress().getHostAddress());
+								aliveMap.put(memAddress, member);
+								DSLogger.report(memAddress,
+										"New member added to the list");
+								continue;
+							}
+
+							else if (aliveMap.containsKey(memAddress)) { // Found
+																			// a
+																			// match
+								DSLogger.log("Receiver", "run",
+										"Found match in alive map for: "
+												+ memAddress);
+								Member localMemberObj = aliveMap
+										.get(memAddress);
+								// This member is leaving the network. Remove it
+								// from alive Map and add it to dead map
+								if (member.getHeartBeat() == -1) {
+									aliveMap.remove(memAddress);
+									deadMap.put(memAddress, member);
+									DSLogger.report(memAddress,
+											" This machine is voluntarily leaving the network");
+									continue;
+								}
+								if (localMemberObj.getHeartBeat() >= member
+										.getHeartBeat()) {
+									// Ignore, as the local member's heartbeat
+									// is
+									// greater than incoming member's heartbeat.
+
+								} else { // Update the local member's heartbeat
+											// with
+											// the
+											// received heartbeat.
+									Member localObj = aliveMap.get(memAddress);
+									localObj.setHeartBeat(member.getHeartBeat());
+									localObj.setTimeStamp(new Date().getTime());
+								}
+							}
+
+							// else if the member was not found in the alive
+							// map,
+							// either
+							// it is a new member or an old update of an already
+							// dead
+							// member
+							else {
+								if (deadMap.containsKey(memAddress)) {
+									// DSLogger.log("Receiver", "run",
+									// "Found match in dead map for: "+memAddress);
+
+									// Check if the local member present in the
+									// dead
+									// Map
+									// has a heartbeat greater than the
+									// heartbeat of
+									// the
+									// received member.
+									Member localMemberObj = deadMap
+											.get(memAddress);
+									if (localMemberObj.getHeartBeat() >= member
+											.getHeartBeat()) {
+										// Ignore, as the local member's
+										// heartbeat
+										// is
+										// greater than incoming member's
+										// heartbeat.
+
+									} else { // False positive!!
+												// remove from dead member list
+												// and add
+												// it
+												// to alive member list.
+										DSLogger.report(memAddress,
+												" False positive detected for this member");
+										Member localObj = deadMap
+												.get(memAddress);
+										localObj.setHeartBeat(member
+												.getHeartBeat());
+										localObj.setTimeStamp(new Date()
+												.getTime());
+										deadMap.remove(memAddress);
+										aliveMap.put(memAddress, localObj);
+
+									}
+								}
+
+								else { // A new member is being added to the
+										// list. (Might include reincarnations
+										// of previously dead members
+									if (!memAddress.startsWith("#")) {
+										DSLogger.log("Receiver", "run",
+												"New member added with "
+														+ memAddress);
+										DSLogger.report(memAddress,
+												"New member added to the list");
+										aliveMap.put(memAddress, member);
+									}
+								}
+							}
+						}
+					}
+					DSLogger.log("Receiver", "run",
+							"********Alive members after update*******");
+					printMemberMap(aliveMap);
+					DSLogger.log("Receiver", "run",
+							"**********Dead members after update*******");
+					printMemberMap(deadMap);
+					DSLogger.log("Receiver", "run", "Lock released by receiver");
+				} else {
+					nodeSocket.close();
+					DSLogger.log("Receiver", "run", "Shutting down reciver");
+					break;
+				}
+
+			} catch (IOException e) {
+				DSLogger.log("Receiver", "run", e.getMessage());
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				DSLogger.log("Receiver", "run", e.getMessage());
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	public DatagramSocket getNodeSocket() {
+		return nodeSocket;
+	}
+
+	public void setNodeSocket(DatagramSocket nodeSocket) {
+		this.nodeSocket = nodeSocket;
+	}
+
+	/* Method to shutdown this thread when the node is leaving the network*/
+	public void shutDown() {
+		String close = new String("close");
+
+		try {
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			ObjectOutputStream oos = new ObjectOutputStream(baos);
+			oos.writeObject(close);
+			DatagramPacket p = new DatagramPacket(baos.toByteArray(),
+					baos.toByteArray().length, InetAddress.getLocalHost(), 3456);
+			nodeSocket.send(p);
+		} catch (UnknownHostException e) {
+			DSLogger.log("Receiver", "run", e.getMessage());
+			e.printStackTrace();
+		} catch (IOException e) {
+			DSLogger.log("Receiver", "run", e.getMessage());
+			e.printStackTrace();
+		}
+	}
+
+	/* Print Gossip method */
+	public void printMemberMap(Map<String, Member> memberMap) {
+
+		Set<String> keys = memberMap.keySet();
+		;
+		Member aMember;
+		for (String key : keys) {
+			aMember = memberMap.get(key);
+			DSLogger.log("Receiver", "printMemberMap ", aMember.getIdentifier());
+		}
+	}
+}
