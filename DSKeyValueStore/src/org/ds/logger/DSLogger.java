c70f1e034975396b66467e3f56df796cf58753b0
paragjain16 <pjain11@illinois.edu>
2013-10-31 17:57:09 -0500

    Added logger

--- DSKeyValueStore/src/org/ds/logger/DSLogger.java
+++ DSKeyValueStore/src/org/ds/logger/DSLogger.java
@@ -9,6 +9,11 @@ public class DSLogger {
 		 public static void log(String className, String methodName, String msg){
 			 log.debug(className+":"+methodName+"~"+msg);
 		 }
+		 static Logger admin = Logger.getLogger("admin");
+		 
+		 public static void logAdmin(String className, String methodName, String msg){
+			 log.debug(className+":"+methodName+"~"+msg);
+		 }
 		 public static void report(String key, String value){
 			 if(value==""){
 				 log.info(key);
